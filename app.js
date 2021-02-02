4fc5c4b20f4844bfa613888fedfcc9f14635373e
MateuszMi <mati_michna@vp.pl>
2021-02-02 20:41:42 +0100

    02.02.2021

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const menu = document.querySelector("#moblie--menu");
-const menuLinks = document.querySelector(".navbar__menu");
+const menuLinks = document.querySelector('.navbar__menu');
 
 
 menu.addEventListener('click', function() {
     this.classList.toggle('is-active');
-    menuLinks.classList.toggle("active");
+    menuLinks.classList.toggle('active');
 });
 
 
 menuLinks.addEventListener('click', function(e) {
-    if (e.target.classList.contains('navbar__links')) {
+    if (e.target.classList.contains('nnavbar__links')) {
         menuLinks.classList.remove('active');
         menu.classList.remove("is-active");
     }
